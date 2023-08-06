fee794f92152849c1fd6105f6c315bfe7f514d69
EndSaidN <saiakbar78@gmail.com>
2023-08-06 08:16:19 +0500

    initial commit

--- accounts/profile_api_url.py
+++
@@ -1,7 +0,0 @@
-from .api import ProfileViewSet
-from rest_framework import routers
-
-profile_router = routers.DefaultRouter()
-profile_router.register('sn/api/profile', ProfileViewSet, 'api-profile')
-
-urlpatterns = profile_router.urls
--- message/post_api_url.py
+++
@@ -1,7 +0,0 @@
-from .api import PostViewSet
-from rest_framework import routers
-
-post_router = routers.DefaultRouter()
-post_router.register('sn/api/posts', PostViewSet, 'api-post')
-
-urlpatterns = post_router.urls
